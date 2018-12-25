5b0bbc9113c0339cfb5c744a533f503b2c009383
CharityQuinn <charityaquinn@gmail.com>
2018-12-25 15:40:32 -0500

    Still revising

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,127 @@
 var plants = ["Jade", "Air Plant", "Christmas Cactus", "Spider"];
 var wins;
 var remainingLetters;
-var lettersGuessed = [];
-var letterBlanks = [];
-var guessArray = [];
+var wordSpell= [];
+var userGuess;
+var guessLeft = 6;
 var randomWord = "";
 
 // Create function to pick the  from list of words
-function inititalizeGame() {
- remainingLetters = plants.length;
- console.log(remainingLetters);
-  randomWord = words[Math.floor((Math.random()=plants.length))];
-  console.log(randomWord);
+
+function initializeGame() {
+
+  // Reset the guesses back to 0.
+  numGuesses = 6;
+  // Create function to pick the  from list of words
+  randomWord = plants[Math.floor(Math.random() * plants.length)];
+  wordSpell = randomWord.split("");
+  numDash = randomWord.length;
+  console.log(" This is the randomWord " + randomWord);
+  goodLtrDashes = [];
+  wrongGuesses = [];
+
+  // Put in the dashes for the letters in chosen plant
+  for (var i = 0; i < numDash; i++) {
+    goodLtrDashes.push("_");
+  }
+
+  console.log("The number of Dashes: " + goodLtrDashes);
+  document.getElementById("guesses").innerHTML = numGuesses;
+
+    document.getElementById("nameSpace").innerHTML = goodLtrDashes.join(" ");
+  document.getElementById("usedLetter").innerHTML = wrongGuesses.join(" ");
 }
-console.log(randomWord);
-  for (var i = 0; i < randomWord.length; i++) {
-    letterBlanks.push("_");
- console.log(plants.length); 
+
+
+function checkLtrs(letter) {
+  console.log("Inside checkLtrs - did I receive a ltr? " + userGuess)
+
+  // a user letter is found anywhere in the word.
+  var letterInWord = false;
+
+  // Check if a letter exists inside the array at all.
+  for (var i = 0; i < numDash; i++) {
+
+    if (randomWord[i] === letter) {
+
+      // If the letter exists then toggle this boolean to true.
+      // This will be used in the next step.
+      letterInWord = true;
+    }
+  }
+
+  // If the letter exists somewhere in the word,
+  // then figure out exactly where (which indices).
+  if (letterInWord) {
+
+    // Loop through the word
+    for (var j = 0; j < numDash; j++) {
+
+      // Populate the goodLtrDashes with every instance of the letter.
+      if (randomWord[j] === letter) {
+
+        // Here we set specific blank spaces to equal the correct letter
+        // when there is a match.
+        goodLtrDashes[j] = letter;
+      }
+    }
+
+    // Log the current blanks and successes for testing.
+    console.log(goodLtrDashes);
+  }
+
+  // If the letter doesn't exist at all...
+  else {
+
+    // Then we add the letter to the list of wrong letters.
+    wrongGuesses.push(letter);
+
+    // We also subtract one of the guesses.
+    numGuesses--;
+
+  }
+
 }
+
+// roundComplete() function
+// Here we will have all of the code that needs to be run after each guess is made.
+function roundComplete() {
+
+    console.log("WinCount: " + wins + "  Gusesses Left: " + guessLeft);
+
+  document.getElementById("guesses").innerHTML = guessLeft;
+  document.getElementById("usedLetter").innerHTML = goodLtrDashes.join(" ");
+
+    if (wordSpell.toString() === goodLtrDashes.toString()) {
+    wins++;
+    alert("You win!");
+
+    initializeGame();
+  } 
+  else if (numGuesses === 0) {
+    lossCounter++;
+    alert("You lose");
+    // Restart the game
+    initializeGame();
+
+  }
+
+}
+
+
+
+initializeGame();
+
 document.onkeyup = function (event) {
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log(userGuess);
-}
-// Set up the answer array
-
-function newFunction() {
-var remainingLetters = newFunction();
-while (remainingLetters > 0) {
-  for (var j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-      answerArray[j] = guess;
-      remainingLetters--;
-    }
-  }
-}
-return word.length;
-}
+ console.log("Did we register a keyboad click? " + event)
+  letterGuessed = String.fromCharCode(event.which).toLowerCase();
+  roundComplete();
+};
 
 
-// If letter matches, replace the dash with the letter user typed 
-function LtrCheck() {
-  for (let i = 0; i < randomWord.length; i++) {
-    if (userGuess == randomWord[i]) {
-      dashes = dashes.replaceAt(i, userGuess);
-      console.log(dashes);
-      wins = wins + 1;
-      alert("You guessed right!");
-    }
-  }
-}
 
-//create character replacement function
-String.prototype.replaceAt = function (index, replacement) {
-  return this.substr(0, index) + replacement + this.substr((index + 1), this.length);
-  console.log()
+function reset() {
+  wins = 0;
+  randomWord = "";
+  remainingLetters = "";
 }
-function reset () {
-wins = 0;
-randomWord = "";
-remainingLetters = "";
-}
-
-
